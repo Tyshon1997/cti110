2bf8148adb1b19564a6f8e50395cdec39eb5c4ed
Tyshon1997 <44030371+Tyshon1997@users.noreply.github.com>
2019-02-12 16:32:29 -0800

    Add files via upload

---
+++ P2T1_SalesPrediction_BenClark.py
@@ -0,0 +1,13 @@
+# A brief description of the project
+# Feb 02,2019
+# CTI-110 P2T1 - Sales Prediction
+# Benjamin Clark
+
+# use value 0.23 for 23%
+
+
+TotalSales = float( input( "Please enter amount of total sales: "))
+
+profit = 0.23 * TotalSales
+
+print( "The profit is $" + format( profit , ",.2f" ))
